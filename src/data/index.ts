885730e82a06d40847c07ecbd66b38f509a3e435
Isrvxl <isrvelverdugo@gmail.com>
2024-12-09 18:39:13 -0300

    Cotizador de Cryptomonedas Project

---
+++ src/data/index.ts
@@ -0,0 +1,8 @@
+import { Currency } from "../types";
+
+export const currencies : Currency[] = [
+  { code: 'USD', name: 'Dolar de Estados Unidos'},
+  { code: 'EUR', name: 'Euro'},
+  { code: 'CLP', name: 'Peso Chileno'},
+  { code: 'MXN', name: 'Peso Mexicano'},
+]
